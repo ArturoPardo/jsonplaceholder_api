97d7f4c53d81b778d7ee8e58a16a9b4e6aad33c0
Arturo Pardo <arturopardosanchez@gmail.com>
2019-03-13 21:47:35 +0100

    fix image random

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,84 +14,84 @@ export class AppComponent {
 
   albums = [];
   posts = [];
-  defecto='';
-  colores=[];
-  clase:string="true";
-username: string = '';
-
-  
+  defecto = '';
+  colores = [];
+  clase: string = "true";
+  username: string = '';
+  comparativa=[];
+mio=[];
+ 
   constructor(private dataservice: DataService) {
-  
-   this.dataservice.getData().subscribe(data => {
-    var random = Math.round(Math.random()*(21-11)+11);
-    this.albums = data.filter(data => data.id < random); 
-    var random2 = Math.round(Math.random()*(random-1)+1);
-      for(let i=1;i<random;i++){    
-        this.colores.push(i*random2);
+
+    this.dataservice.getData().subscribe(data => {
+      var random = Math.round(Math.random() * (21 - 11) + 11);
+      // this.albums = data.filter(data => data.id < random);
+     
+      for (let i = 1; i < random; i++) {
+        var random2 = Math.round(Math.random() * (data.length- i) + i);
+        this.comparativa.push(random2);
+        this.colores.push(random2);
+      } 
+
+
+
+      let arreglo = data;
+      for (let i=0;i<this.comparativa.length;i++){
+        let busqueda = this.comparativa[i];
+              let indice = arreglo.find(data => data.id ==busqueda);
+              console.log(indice);
+              this.mio.push(indice);
        
+      
+      
       }
-      console.log("imagenes",  this.albums.length);
-      console.log ("colores",JSON.stringify(this.colores));
-   
+
     })
-    
-   
+
+
 
     this.dataservice.getDato().subscribe(data => {
-    
-    
+
+
       this.posts = data.filter(data => data.body.match(this.defecto));
       console.log("post", this.posts.length);
-     
-     
 
-      for(let i=0;i<this.posts.length;i++){
-        
-        
-        if (i%2!=0){console.log("impar", i);
+
+
+      for (let i = 0; i < this.posts.length; i++) {
+
+
+        if (i % 2 != 0) {
+          console.log("impar", i);
+        } else {
+          console.log("par", i);
         }
-        else{  console.log("par", i);
-        }
-        
-      
-      
+
       }
-      
-      
-    
-    
-   
+
     })
-  
+
   }
 
   albumFinder() {
-  //  ev++;
-  //   this.dataservice.getDato().subscribe(data => {
-  //     this.posts = data.filter(data => data.id < ev);
-   
-  //     console.log("post", this.posts.length);
-  //   })
-var query = this.username;
+  
+    var query = this.username;
     this.dataservice.getDato().subscribe(data => {
-      this.posts = data.filter(data => data.body.match(query));
+        this.posts = data.filter(data => data.body.match(query));
 
 
+      }
 
-
-
- }
-     
     )
 
-  
+
   }
-  borrar(){
+  borrar() {
 
     var query = '';
     this.dataservice.getDato().subscribe(data => {
       this.posts = data.filter(data => data.body.match(query));
-     
+
       console.log("post", this.posts.length);
     })
 
