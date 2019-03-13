a41d500e23d83bd09ac8ae8e6372611e0f4481db
Arturo Pardo <arturopardosanchez@gmail.com>
2019-03-13 18:28:48 +0100

    random function & title finder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
 import {
   DataService
 } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,32 +15,86 @@ export class AppComponent {
   alumnos = ['mariela', 'poppy'];
   albums = [];
   posts = [];
-
-  defecto=5;
+  defecto='';
+  colores=[];
+  clase:string="true";
 
   
   constructor(private dataservice: DataService) {
- 
+  
    this.dataservice.getData().subscribe(data => {
-    this.albums = data.filter(data => data.id < 21);
-    console.log("imagenes",  this.albums.length);
+    var random = Math.round(Math.random()*(21-11)+11);
+    this.albums = data.filter(data => data.id < random); 
+    var random2 = Math.round(Math.random()*(random-1)+1);
+      for(let i=1;i<random;i++){    
+        this.colores.push(i*random2);
+       
+      }
+      console.log("imagenes",  this.albums.length);
+      console.log ("colores",JSON.stringify(this.colores));
+   
     })
     
-    this.dataservice.getDato().subscribe(data => {
-     
-      this.posts = data.filter(data => data.id < this.defecto);
-      console.log("post", this.posts.length);
-    })
+   
 
+    this.dataservice.getDato().subscribe(data => {
+    
+    
+      this.posts = data.filter(data => data.body.match(this.defecto));
+      console.log("post", this.posts.length);
+     
+     
+
+      for(let i=0;i<this.posts.length;i++){
+        
+        
+        if (i%2!=0){console.log("impar", i);
+        }
+        else{  console.log("par", i);
+        }
+        
+      
+      
+      }
+      
+      
+    
+    
+   
+    })
+  
   }
 
-  albumFinder(ev:number) {
-   ev++;
+  albumFinder(ev:string) {
+  //  ev++;
+  //   this.dataservice.getDato().subscribe(data => {
+  //     this.posts = data.filter(data => data.id < ev);
+   
+  //     console.log("post", this.posts.length);
+  //   })
+var query = ev;
     this.dataservice.getDato().subscribe(data => {
-      this.posts = data.filter(data => data.id < ev);
+      this.posts = data.filter(data => data.body.match(query));
+
+
+
+
+
+ }
+     
+    )
+
+  
+  }
+  borrar(){
+
+    var query = '';
+    this.dataservice.getDato().subscribe(data => {
+      this.posts = data.filter(data => data.body.match(query));
+     
       console.log("post", this.posts.length);
     })
-  
-  
+
+
   }
 }
